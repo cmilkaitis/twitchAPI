649cecf30efc97efbdafc3d8dd6188d1644b878c
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-24 14:03:58 -0400

    Added images

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     let url = "https://wind-bow.glitch.me/twitch-api/streams/";
     let channel = "https://wind-bow.glitch.me/twitch-api/channels/";
     
-    for(let i = 0; i < streams.length; i++) {
+    for(let i in streams) {
        let streamUrl = url + streams[i];
        let channelUrl = channel + streams[i];
 
@@ -12,12 +12,18 @@ $(document).ready(function() {
                 $.getJSON(channelUrl, function(data){
                     console.log(data);
                     let date = (String(data.updated_at).split("").slice(0,10).join(""));
-                    $("#content").append("<div class='item' style='border: black 1px solid; margin-top: 5px; margin-bottom: 5px'><p>" + data.display_name +" was last online " + date + "</p></div>");
+                    $("#content").append(`<a id='a-${streams[i]}'href=''><div id='${streams[i]}' style='display: block; border: black 1px solid; margin-top: 5px; margin-bottom: 5px'></div></a>`);
+                    $(`#a-${streams[i]}`).attr('href', data.url); 
+                    $(`#${streams[i]}`).append(`<img style='max-width: 150px;' src="${data.logo}">`);
+                    $(`#${streams[i]}`).append(`<p style='display: block'>${data.display_name} was last online ${date}</p>`);
                 });
             } else {
-                $("#content").append(`<div id='channel${i}' class='item' style='border: black 1px solid; margin-top: 5px; margin-bottom: 5px'><p> ${streams[i]} is online playing ${data.stream.channel.game} for ${data.stream.viewers} viewers.<p></div>`);
-                $(`#channel${i}`).append(`<img src="${data.stream.preview.medium}">`)
-            }   
+                console.log(data);
+                $("#content").append(`<a id='a-${streams[i]} href='' ><div id='${streams[i]}' style='border: black 1px solid; margin-top: 5px; margin-bottom: 5px'></div></a>`);
+                $(`#a-${streams[i]}`).attr('href', data.stream.channel.url); 
+                $(`#${streams[i]}`).append(`<img style='max-width: 150px;' src="${data.stream.channel.logo}">`);
+                $(`#${streams[i]}`).append(`<p> ${data.stream.channel.display_name} is online playing ${data.stream.channel.game} for ${data.stream.viewers} viewers.<p>`);
+            }  
         });
     }
 });
