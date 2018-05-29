9916fb34d16a2c78910ee4ed41b96cde98a0377e
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-29 12:42:23 -0400

    Added styles

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ $(document).ready(function() {
             if(data.stream === null) {
                 $.getJSON(channelUrl, function(data){
                     let date = (String(data.updated_at).split("").slice(0,10).join(""));
-                    $("#content").append(`<a id='a-${streams[i]}'href=''><div id='${streams[i]}' style='background: white; border-radius: 5px; display: block; margin-top: 5px; margin-bottom: 5px; padding-left: 10px; padding-top: 10px'></div></a>`);
+                    $("#content").append(`<a id='a-${streams[i]}'href=''><div id='${streams[i]}' style='-webkit-box-shadow: 0 10px 5px -7px #dc4b5f; -moz-box-shadow: 0 10px 5px -7px #dc4b5f; box-shadow: 0 10px 5px -7px #dc4b5f; background: white; display: block; margin-top: 10px; margin-bottom: 10px; padding-left: 10px; padding-top: 10px'></div></a>`);
                     $(`#a-${streams[i]}`).attr('href', data.url); 
                     $(`#${streams[i]}`).append(`<img style='max-width: 150px; margin-bottom: 10px' src="${data.logo}">`);
                     $(`#${streams[i]}`).append(`<p>${data.display_name} was last online ${date}</p>`);
                 });
             } else {
-                $("#content").append(`<a id='a-${streams[i]}' href=''><div id='${streams[i]}' style='background: white; border-radius: 5px; margin-top: 5px; margin-bottom: 5px; padding-left: 10px; padding-top: 10px''></div></a>`);
+                $("#content").append(`<a id='a-${streams[i]}' href=''><div id='${streams[i]}' style=' -webkit-box-shadow: 0 10px 5px -7px #3bd264; -moz-box-shadow: 0 10px 5px -7px #3bd264; box-shadow: 0 10px 5px -7px #3bd264; background: white; margin-top: 10px; margin-bottom: 10px; padding-left: 10px; padding-top: 10px''></div></a>`);
                 $(`#a-${streams[i]}`).attr('href', data.stream.channel.url); 
                 $(`#${streams[i]}`).append(`<img style='max-width: 150px; margin-bottom: 10px' src="${data.stream.channel.logo}">`);
                 $(`#${streams[i]}`).append(`<p> ${data.stream.channel.display_name} is online playing ${data.stream.channel.game} for ${data.stream.viewers} viewers.<p>`);
